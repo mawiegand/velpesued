88dcd6d166335e166f07a4aeb27f7fc034d84f52
Marcel Wiegand <marcel@bluepanel.org>
2018-07-17 16:26:38 +0200

    Update locale config

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -5,9 +5,9 @@ Refinery::I18n.configure do |config|
 
   config.current_locale = :de
 
-  # config.default_frontend_locale = :en
+  config.default_frontend_locale = :de
 
-  # config.frontend_locales = [:en]
+  config.frontend_locales = [:de]
 
   # config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :pt=>"Português", :"pt-BR"=>"Português brasileiro", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :ca=>"Català", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"简体中文", :"zh-TW"=>"繁體中文", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български", :hu=>"Hungarian", :uk=>"Українська"}
 end
