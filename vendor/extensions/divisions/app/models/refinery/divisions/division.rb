1ed4d7cb3b1ba78faaa220cae349c5a8cd355c1f
Marcel Wiegand <marcel@bluepanel.org>
2018-08-03 18:45:59 +0200

    Enable search for divisions extension
    Related to #7.

--- vendor/extensions/divisions/app/models/refinery/divisions/division.rb
+++ vendor/extensions/divisions/app/models/refinery/divisions/division.rb
@@ -7,8 +7,7 @@ module Refinery
       validates :name, :presence => true, :uniqueness => true
 
       # To enable admin searching, add acts_as_indexed on searchable fields, for example:
-      #
-      #   acts_as_indexed :fields => [:title]
+      acts_as_indexed :fields => [:name]
 
     end
   end
