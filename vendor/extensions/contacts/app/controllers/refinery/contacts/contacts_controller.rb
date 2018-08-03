a94b964b8f900c49aead12d6240d1241670393f6
Marcel Wiegand <marcel@bluepanel.org>
2018-08-03 02:05:53 +0200

    Contacts: remove show action and view
    Related to #5.

--- vendor/extensions/contacts/app/controllers/refinery/contacts/contacts_controller.rb
+++ vendor/extensions/contacts/app/controllers/refinery/contacts/contacts_controller.rb
@@ -11,14 +11,6 @@ module Refinery
         present(@page)
       end
 
-      def show
-        @contact = Contact.find(params[:id])
-
-        # you can use meta fields from your model instead (e.g. browser_title)
-        # by swapping @page for @contact in the line below:
-        present(@page)
-      end
-
     protected
 
       def find_all_contacts
