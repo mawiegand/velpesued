6c7ebdaaf5cd6f5d5ac16e844258790317bb738f
Marcel Wiegand <marcel@bluepanel.org>
2018-08-06 23:33:33 +0200

    Divisions: fix group routes
    Related to #7.

--- vendor/extensions/divisions/config/routes.rb
+++ vendor/extensions/divisions/config/routes.rb
@@ -2,6 +2,7 @@ Refinery::Core::Engine.routes.draw do
 
   # Frontend routes
   namespace :divisions do
+    resources :groups, :only => [:index, :show]
     resources :divisions, :path => '', :only => [:index, :show]
   end
 
@@ -14,16 +15,7 @@ Refinery::Core::Engine.routes.draw do
         end
       end
     end
-  end
-
-
-  # Frontend routes
-  namespace :divisions do
-    resources :groups, :only => [:index, :show]
-  end
-
-  # Admin routes
-  namespace :divisions, :path => '' do
+    
     namespace :admin, :path => "#{Refinery::Core.backend_route}/divisions" do
       resources :groups, :except => :show do
         collection do
