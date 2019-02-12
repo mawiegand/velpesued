aa107c15734eaeacbbe82837e2b8f71270070b79
Marcel Wiegand <marcel@bluepanel.org>
2019-02-13 00:47:28 +0100

    Divisions: add full_name method to group
    * Display full_name in admin group list view.
    * Use full_name of group in select field of positions form view.
    
    Related to #7 and #13.

--- vendor/extensions/divisions/app/models/refinery/divisions/group.rb
+++ vendor/extensions/divisions/app/models/refinery/divisions/group.rb
@@ -13,6 +13,10 @@ module Refinery
       # To enable admin searching, add acts_as_indexed on searchable fields, for example:
       acts_as_indexed :fields => [:name]
 
+      def full_name
+        [name, division&.name].join(' ')
+      end
+
       def should_generate_new_friendly_id?
         name_changed? || super
       end
